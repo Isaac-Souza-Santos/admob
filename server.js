d9e615e155c63e4fade5f63de2a4d36c2243012d
Seu Nome Completo <souza-sjc@hotmail.com>
2025-09-16 16:54:18 -0300

    Remove Google site verification route and clean up comments in server.js for clarity

--- server.js
+++ server.js
@@ -4,17 +4,12 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Servir arquivos estáticos da raiz
+// Servir arquivos estáticos
 app.use(express.static("."));
 
 // Rota principal - servir o index.html
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "index.html"));
-});
-
-// Rota para o arquivo de verificação do Google
-app.get("/google6ea14f55b40fafcd.html", (req, res) => {
-  res.sendFile(path.join(__dirname, "google6ea14f55b40fafcd.html"));
 });
 
 // Rota para todas as outras requisições - redirecionar para o index.html (SPA)
